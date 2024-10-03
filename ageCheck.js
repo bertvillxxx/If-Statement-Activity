34fa420ce1bb8eb64703129fcb10a9a17c3454a4
Villanueva, Robert G. <bertvillxxxxva@gmail.com>
2024-10-03 06:41:12 -0800

    Add files via upload

---
+++ ageCheck.js
@@ -0,0 +1,11 @@
+let age = 35;
+let enterYourAge = age
+
+console.log("Enter Your Age:", enterYourAge);
+
+if(age < 13)
+{console.log("You are a Child.");} 
+if(age < 20)
+{console.log("You Are a Teenager.");} 
+else
+{console.log("You are an Adult.");}
